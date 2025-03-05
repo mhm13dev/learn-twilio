f95fa049fefdd804c13b583431b8399a19bd1c10
Mubashir Hassan <mhm13dev@gmail.com>
2025-03-06 04:02:35 +0500

    feat: removed dotenv in favor of Bun.env

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import "dotenv/config";
 import { Twilio } from "twilio";
 
 const client = new Twilio(
-  process.env.TWILIO_API_KEY_SID,
-  process.env.TWILIO_API_KEY_SECRET,
-  { accountSid: process.env.TWILIO_ACCOUNT_SID }
+  Bun.env.TWILIO_API_KEY_SID,
+  Bun.env.TWILIO_API_KEY_SECRET,
+  { accountSid: Bun.env.TWILIO_ACCOUNT_SID }
 );
 
-const to = process.env.TWILIO_TO_PHONE_NUMBER!;
-const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID!;
+const to = Bun.env.TWILIO_TO_PHONE_NUMBER!;
+const messagingServiceSid = Bun.env.TWILIO_MESSAGING_SERVICE_SID!;
 const body = "Hello from Twilio!";
 
 client.messages
