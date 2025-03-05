1a53427a33a3016fb3a0838348dcea9c12f02511
Mubashir Hassan <mhm13dev@gmail.com>
2025-03-06 03:06:49 +0500

    feat: send sms with twilio api key using phone number

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import "dotenv/config";
+import { Twilio } from "twilio";
+
+const client = new Twilio(
+  process.env.TWILIO_API_KEY_SID,
+  process.env.TWILIO_API_KEY_SECRET,
+  { accountSid: process.env.TWILIO_ACCOUNT_SID }
+);
+
+const to = process.env.TWILIO_TO_PHONE_NUMBER!;
+const from = process.env.TWILIO_FROM_PHONE_NUMBER!;
+const body = "Hello from Twilio!";
+
+client.messages
+  .create({
+    to,
+    from,
+    body,
+  })
+  .then((message) => console.log(message))
+  .catch((error) => console.error(error));
